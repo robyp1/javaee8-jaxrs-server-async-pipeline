7d9b3ef5aceeedc221ff81c5c9e076e3a6b3f528
Roby <roberto.portoni@gmail.com>
2019-08-08 22:05:21 +0200

    esempio di builder pattern funzionale con labda

--- src/main/java/com/wordpress/abhirockzz/javaee8/builder/mainBuilderTest.java
+++ src/main/java/com/wordpress/abhirockzz/javaee8/builder/mainBuilderTest.java
@@ -1,8 +1,16 @@
 package com.wordpress.abhirockzz.javaee8.builder;
 
+import java.text.ParseException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.LongStream;
+
+import static java.util.stream.Collectors.toMap;
+
 public class mainBuilderTest {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws ParseException {
         Person person = new PersonBuilderLambda().add(
                 $ -> {
                     $.setName("Bob");
@@ -10,12 +18,78 @@ public class mainBuilderTest {
                     $.setPhone("123");
                 }).add(
                 $ ->
-                    $.setAdress( new PersonBuilderLambda.AddressBuilderLambda().add(
-                            addressBuilder -> {
-                                addressBuilder.setCity("New York");
-                                addressBuilder.setStreet("XX");
-                            }).build()//create an Address to add to person
-                    )).build();//create a Person
+                        $.setAdress(new PersonBuilderLambda.AddressBuilderLambda().add(
+                                addressBuilder -> {
+                                    addressBuilder.setCity("New York");
+                                    addressBuilder.setStreet("XX");
+                                }).build()//create an Address to add to person
+                        )).build();//create a Person
         System.out.println(person);
+
+        //test heap, meanwwhile it run launch jvm profiler
+        final Map<Long, FillMaps.Point> m = LongStream.range(0, 1_000_000)
+                .boxed()
+                .collect(
+                        toMap(
+                                Function.identity(),
+                                FillMaps::pointFrom,
+                                (u, v) -> {
+                                    throw new IllegalStateException();
+                                },
+                                HashMap::new
+                        )
+                );
+
+//        String num = "123,90";
+//        NumberFormat nbf = NumberFormat.getInstance();
+//        Number number = nbf.parse(num);
+//        BigDecimal bigDecimal = new BigDecimal(number.toString()).setScale(2);
+//        System.out.println(bigDecimal);
+
+    }
+
+    final static class FillMaps {
+
+
+        // Conveniency method that creates a Point from
+        // a long by applying modulo prime number operations
+        private static Point pointFrom(long seed) {
+            final Point point = new Point();
+            point.setX((int) seed % 4517);
+            point.setY((int) seed % 5011);
+            return point;
+        }
+
+
+        final static class Point {
+            int x;
+            int y;
+
+            public Point() {
+                this.x = 0;
+                this.y = 0;
+            }
+
+            public Point(int x, int y) {
+                this.x = x;
+                this.y = y;
+            }
+
+            public int getX() {
+                return x;
+            }
+
+            public void setX(int x) {
+                this.x = x;
+            }
+
+            public int getY() {
+                return y;
+            }
+
+            public void setY(int y) {
+                this.y = y;
+            }
+        }
     }
 }
